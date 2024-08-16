bf19922a0f0953a7549a1bbce9c20d35c01755aa
Liam263 <nguyenthanhlam091@gmail.com>
2024-08-17 04:10:44 +1000

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,176 @@
-import Image from "next/image";
+"use client";
+import { Download } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import html2canvas from "html2canvas";
+import { Button } from "@/components/ui/button";
+import { TiCamera } from "react-icons/ti";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function Home() {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const canvasContainerRef = useRef<HTMLDivElement | null>(null);
+  const [stream, setStream] = useState<MediaStream | null>(null);
+  const [canvases, setCanvases] = useState<HTMLCanvasElement[]>([]);
+  const maxCanvases = 4;
+  const [background, setBackground] = useState('bg-blue-500')
+  const currentDate = new Date();
+  const formatDate = (date: Date) => {
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = String(date.getFullYear()).slice(-2); // Get last two digits of the year
+
+    return `${day}.${month}.${year}`;
+  };
+
+  const handleChange = (value: string) => {
+    setBackground(value ); 
+  };
+
+  const streamVideo = async () => {
+    try {
+      const mediaStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+      if (videoRef.current) {
+        videoRef.current.srcObject = mediaStream;
+        videoRef.current.play(); // Ensure the video plays
+      }
+      setStream(mediaStream);
+    } catch (error) {
+      console.error("Error accessing webcam: ", error);
+    }
+  };
+
+  const handleClick = () => {
+    const canvas = document.createElement("canvas");
+    canvas.width = 640;
+    canvas.height = 480;
+    const context = canvas.getContext("2d");
+
+    if (context && videoRef.current) {
+      // Draw the current video frame on the new canvas
+      context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+    }
+
+    // Update the list of canvases
+    setCanvases((prevCanvases) => {
+      const updatedCanvases = [...prevCanvases, canvas];
+      if (updatedCanvases.length > maxCanvases) {
+        updatedCanvases.shift(); // Remove the oldest canvas if limit is exceeded
+      }
+      return updatedCanvases;
+    });
+  };
+
+  const handleDownload = async () => {
+    if (canvasContainerRef.current) {
+      const canvas = await html2canvas(canvasContainerRef.current);
+      const link = document.createElement("a");
+      link.href = canvas.toDataURL("image/jpeg");
+      link.download = "webcam.jpg";
+      link.click();
+    }
+  };
+
+  const handleClearImages = () => {
+    setCanvases([]);
+  };
+  useEffect(() => {
+    streamVideo();
+    return () => {
+      if (stream) {
+        stream.getTracks().forEach((track) => track.stop());
+      }
+    };
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="container flex flex-row  justify-center space-x-10 h-full">
+      <div className="flex flex-col items-center justify-center space-y-5">
+        <p className="font-bold text-6xl">Photobooth</p>
+        <video
+          ref={videoRef}
+          className="bg-gray-400"
+          width="640"
+          height="480"
+          autoPlay
+          muted
+        ></video>
+        <div className="mt-4 flex flex-col items-center justify-center gap-4">
+          <div className="bg-red-500 p-3 rounded-full">
+            <TiCamera
+              onClick={handleClick}
+              className="cursor-pointer text-4xl text-slate-50"
             />
-          </a>
+          </div>
+          {canvases.length > 0 && (
+            <div className="flex flex-row gap-2">
+              <Button onClick={handleDownload} className="gap-2">
+                Download
+                <Download className="w-4 h-4" />
+              </Button>
+              <Button className="" onClick={handleClearImages}>
+                Clear Images
+              </Button>
+              <Select onValueChange={handleChange}>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Select background" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="bg-blue-500">Light Blue</SelectItem>
+                  <SelectItem value="bg-black">Black</SelectItem>
+                  <SelectItem value="bg-gray-300">White</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          )}
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* Display the list of canvases */}
+      <div
+        ref={canvasContainerRef}
+        className={`${
+          canvases.length > 0 ? `flex flex-col gap-2 mt-4 p-2 ${background}` : ""
+        }`}
+      >
+        {canvases.map((canvas, index) => (
+          <div key={index} className="relative ">
+            <canvas
+              width="200"
+              height="150"
+              className="w-full h-full"
+              ref={(ref) => {
+                if (ref) {
+                  const ctx = ref.getContext("2d");
+                  if (ctx) {
+                    ctx.drawImage(canvas, 0, 0, ref.width, ref.height);
+                  }
+                }
+              }}
+            />
+          </div>
+        ))}
+        {canvases.length === 4 && (
+          <div className="flex flex-col items-end justify-center">
+            <input
+              className={ background === 'bg-black'? 'text-white bg-transparent border-none font-bold placeholder:text-slate-500 text-left text-lg': `bg-transparent border-none font-bold placeholder:text-slate-500 text-left text-lg`}
+              placeholder="Happy Birthday"
+            />
+            <div className={background === 'bg-black'? 'font-semibold text-sm text-white': `font-semibold text-sm`}>to ____________________</div>
+            <div className={background === 'bg-black'? 'text-sm text-white':"text-sm text-slate-500"}>
+              {formatDate(currentDate)}
+            </div>
+          </div>
+        )}
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
